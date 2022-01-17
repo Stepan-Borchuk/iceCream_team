862fad305536a4a265b12ecd31a4801fa58708ae
Yuriy Pokhyl <woully@gmail.com>
2022-01-17 11:03:35 +0200

    main-fix

--- src/js/gallery-viev.js
+++ src/js/gallery-viev.js
@@ -11,11 +11,4 @@
     function toggleMenu() {
       refs.menu.classList.toggle('is-hidden');
     }
-  //   $('.gallery-viev__list').slick({
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   fade: true,
-  //   cssEase: 'linear'
-  // });
   })();
